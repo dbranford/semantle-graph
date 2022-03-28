04168b854fea1b81299c2763465cb2a4e05571d7
dbranford <25404972+dbranford@users.noreply.github.com>
2022-03-28 23:33:20 +0100

    Add delay to bookmarklet to avoid graph over-writing

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,5 @@
 javascript:
+(() => {
 graphWindow = window.open("https://dbranford.github.io/semantle-graph/index.html", "graph");
-graphWindow.postMessage(localStorage.getItem("guesses"), "https://dbranford.github.io");
+setTimeout(() => {graphWindow.postMessage(localStorage.getItem("guesses"), "https://dbranford.github.io");}, 1000);
+})();
