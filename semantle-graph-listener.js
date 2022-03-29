4ef81329fb4468948003b83cf40c87429e3ead0f
dbranford <25404972+dbranford@users.noreply.github.com>
2022-03-29 21:36:34 +0100

    Add re-plot button with score/proximity toggle (trace is only shown for consecutive top-1000)

--- semantle-graph-listener.js
+++ semantle-graph-listener.js
@@ -1,6 +1,10 @@
 window.addEventListener("message", (event) => {
-guesses = JSON.parse(event.data);
-guesses.sort((a,b) => a[3]-b[3]);
-var progress = guesses[0].map((col, i) => guesses.map(row => row[i]));
-window.localStorage.setItem("progress", JSON.stringify(progress));
+  let guesses = JSON.parse(event.data);
+  guesses.sort((a,b) => a[3]-b[3]);
+  var progress = guesses[0].map((col, i) => guesses.map(row => row[i]));
+  window.localStorage.setItem("progress", JSON.stringify(progress));
+  if (drawn==true) {
+    plot_replot();
+		plot_status.textContent = "New data received.";	
+  }
 });
--- semantle-graph.js
+++ semantle-graph.js
@@ -1,16 +1,56 @@
-const storage = window.localStorage;
+var progress = JSON.parse(window.localStorage.getItem("progress"));
+var plot_data = new Array();
+var plot_state;
 
-function plot(guesses, elem) {
-	Plotly.newPlot( elem, [{
-		x: guesses[3],
-		y: guesses[0] }],
+function plot(plot_data) {
+	Plotly.newPlot( plot_canvas, [{
+		x: plot_data[0],
+		y: plot_data[1] }],
+		{ margin: { t: 0 } }
+	);
+	var drawn = true;
+}
+
+function plot_replot(plot_data) {
+	Plotly.newPlot( plot_canvas, [{
+		x: plot_data[0],
+		y: plot_data[1] }],
 		{ margin: { t: 0 } }
 	);
 }
 
-var progress = JSON.parse(storage.getItem("progress"));
-if (progress != null){
-window.addEventListener("load", (event) => {
-plot(progress, document.getElementById("semantle-plot"));
-});
+function plot_button() {
+	plot_status.textContent="Re-plotting";
+	plot_replot(progress)
+	plot_status.textContent="Re-plotted";
 }
+
+window.addEventListener("load", (event) => {
+	plot_status = document.getElementById("semantle-graph-status")
+	plot_canvas = document.getElementById("semantle-plot")
+	form = document.querySelector("form");
+	button = document.querySelector("button");
+
+	if (progress == null){
+		plot_status.textContent = "Waiting for data...";	
+		var plot_data = [[0],[0]];
+	} else {
+		plot_status.textContent = "Data found.";	
+		var plot_data = [progress[3],progress[0]];
+	}
+
+	plot(plot_data);
+
+	button.addEventListener("click", (event) => {
+		var data = new FormData(form);
+		let quantity = data.get("quantity");
+		if (quantity=="score") {
+			plot_status.textContent = "Re-plotting score";	
+			plot_data[1] = progress[0];
+		} else if (quantity=="proximity") {
+			plot_status.textContent = "Re-plotting proximity";	
+			plot_data[1] = progress[2];
+		}
+		plot_replot(plot_data)
+	});
+});
