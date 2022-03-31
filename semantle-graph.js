77f103e76879933d3ccd9a581101a2d17fc2545a
dbranford <25404972+dbranford@users.noreply.github.com>
2022-03-31 19:12:00 +0100

    Store data to be plotted in variable and update to change plot

--- semantle-graph.js
+++ semantle-graph.js
@@ -1,6 +1,9 @@
 var progress = JSON.parse(window.localStorage.getItem("progress"));
 var plot_data = new Array();
-var plot_state;
+var plot_state = {
+	quantity: "score",
+	measure: "value",
+};
 
 function plot(plot_data) {
 	Plotly.newPlot( plot_canvas, [{
@@ -25,6 +28,14 @@ function plot_button() {
 	plot_status.textContent="Re-plotted";
 }
 
+function set_plot_data(plot_state) {
+		if (plot_state.quantity=="score") {
+			plot_data[1] = progress[0];
+		} else if (plot_state.quantity=="proximity") {
+			plot_data[1] = progress[2];
+		}
+}
+
 window.addEventListener("load", (event) => {
 	plot_status = document.getElementById("semantle-graph-status")
 	plot_canvas = document.getElementById("semantle-plot")
@@ -44,12 +55,12 @@ window.addEventListener("load", (event) => {
 	button.addEventListener("click", (event) => {
 		var data = new FormData(form);
 		let quantity = data.get("quantity");
+		plot_state.quantity = data.get("quantity");
+		set_plot_data(plot state);
 		if (quantity=="score") {
 			plot_status.textContent = "Re-plotting score";	
-			plot_data[1] = progress[0];
 		} else if (quantity=="proximity") {
 			plot_status.textContent = "Re-plotting proximity";	
-			plot_data[1] = progress[2];
 		}
 		plot_replot(plot_data)
 	});
